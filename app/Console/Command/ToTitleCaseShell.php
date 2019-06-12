43674ebf1d90783c8314a0c45290d820095d880e
Sebastian Perez Leale <sebartista@live.com>
2019-06-12 03:31:52 -0300

    to title completo

--- app/Console/Command/ToTitleCaseShell.php
+++ app/Console/Command/ToTitleCaseShell.php
@@ -22,19 +22,39 @@ class ToTitleCaseShell extends AppShell {
     
     private function updateComercios(){
         $comercio = $this->Comercio->find('first', array(
-            'conditions' => array('Comercio.id !=' => $this->last_id),
-            'fields' => array('Comercio.id'),
+            'conditions' => array('Comercio.id !=' => $this->last_id),            
             'order' => array('Comercio.id ASC')
         ));
-        //debug($comercio);
+        
         if($comercio){
             
-            $this->Comercio->read('nombrefantasia', $comercio['Comercio']['id']);
-            $this->out($this->Comercio->nombrefantasia);
-//            $this->last_id[] = $comercio['Comercio']['id'];
+            $this->Comercio->read(null, $comercio['Comercio']['id']);
+            
+            $this->Comercio->set('razonsocial', ucwords(strtolower($this->remove_bs($comercio['Comercio']['razonsocial']))));
+            $this->Comercio->set('direccion', ucwords(strtolower($this->remove_bs($comercio['Comercio']['direccion']))));
+            $this->Comercio->set('nombrefantasia', ucwords(strtolower($this->remove_bs($comercio['Comercio']['nombrefantasia']))));
+            $this->Comercio->save();
+            // $this->out(ucwords(strtolower($comercio['Comercio']['razonsocial'])));
+            // $this->out(ucwords(strtolower($comercio['Comercio']['direccion'])));
+            // $this->out(ucwords(strtolower($comercio['Comercio']['nombrefantasia'])));
+            $this->out('saving '.$comercio['Comercio']['id']);
+
+            $this->last_id[] = $comercio['Comercio']['id'];
 //            //$this->out($this->last_id);
-//            $this->updateComercios();
+            $this->updateComercios();
         }
+    }
+
+    private function remove_bs($Str) {  
+      $StrArr = str_split($Str); $NewStr = '';
+      foreach ($StrArr as $Char) {    
+        $CharNo = ord($Char);
+        if ($CharNo == 163) { $NewStr .= $Char; continue; } // keep £ 
+        if ($CharNo > 31 && $CharNo < 127) {
+          $NewStr .= $Char;    
+        }
+      }  
+      return $NewStr;
     }
 
 }
