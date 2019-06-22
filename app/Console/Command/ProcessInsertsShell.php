e6b468c973d19ca2e3ceba62a228dd8306cd8a0f
Sebastian Perez Leale <sebartista@gmail.com>
2019-06-21 23:08:12 -0300

    procesos de inserts

--- app/Console/Command/ProcessInsertsShell.php
+++ app/Console/Command/ProcessInsertsShell.php
@@ -1,78 +1,89 @@
 <?php
 
-App::uses('Folder', 'Utility');
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * CakePHP ProcessInserts
+ * @author sebartista
+ */
 App::uses('File', 'Utility');
 
-class TransactionIntegrationShell extends AppShell {
+class ProcessInsertsShell extends AppShell {
 
     public $uses = array('Comercio', 'Ciudade');
     private $rubros_reference = array();
     private $provincias_reference = array();
-    private $data_with_errors_file;
+    private $inserts_file;
 
     public function main() {
-        $this->populateRubros();
-        $this->populateProvinces();
-        $this->data_with_errors_file = new File('/var/www/html/ahora12admin2x/app/webroot/files/with_errors'.date("Y-m-d-h-i-s").'.csv', true);
-        $this->data_with_errors_file->open("w");
+        $this->_populateRubros();
+        $this->_populateProvinces();
+        //$this->inserts_file = new File('/var/www/html/ahora12admin2x/app/webroot/files/inserts_2019-06-21-19-01-03-31.csv', true);
         //$this->out("test write file ".$this->data_with_errors_file->append("test"));
-        $this->processComerces();
+        $this->_processFile();
     }
 
-    private function processComerces() {
+    private function _processFile() {
         $this->stdout->styles('flashy', array('text' => 'green', 'blink' => true));
 
         $this->out('<flashy>working</flashy>');
-        $dir = new Folder('C:\Users\sebar\Downloads\ahora12', false);
-        $file = new File('/var/www/html/ahora12admin2x/app/webroot/files/comercios_junio2018_2019.csv', false);
+
+        $file = new File('/var/www/html/ahora12admin2x/app/webroot/files/inserts_2019-06-21-06-55-48-segundamitad.csv', false);
 
         $file->open('r');
-        //$contents = $file->read();
+
         $this->out('file opened');
-        $find_counter = 0;
+        $line_counter = 0;
         while (($line = fgets($file->handle)) !== false) {
             $exploded = explode("|", $line);
+            $line_counter++;
 
-            if ($exploded[0] != '"cuit"') {
-                $this->processLine($exploded);
-            }
+            $this->_processLine($exploded);
         }
-        $this->out("encontre " . $find_counter);
-
-
         $file->close();
     }
 
-    private function insertNewComerce($data) {
-        //no existe cuit
+    private function _processLine($data) {
+
+        $this->_insertNewComerce($data);
+    }
+
+    private function _insertNewComerce($data) {
+
         foreach ($data as &$d) {
             $d = str_replace('"', '', $d);
         }
-        //$this->out("<flashy>no existe comercio</flashy> " . $data[0]);
-        $localidad = $this->getLocalidad($data[5]);
-        if (!$localidad) {
-            $this->data_with_errors_file->append(trim(implode("|", $data) )."\r\n");
-        } else {
+        //condicion si la provincia no es nula, y el rubro es mayor a 100, sino dato mal cargado
+        if ($data[6] > 0 && $data[1] > 99) {
+            $localidad = $this->_getLocalidad($data[5]);
+            
+            $rubro = $this->_getRubroReference($data[1]);
+            $provincia_id = $this->_getProvinciaReference(trim($data[6]));
             //insert
             $new_comerce = array(
                 "cuit" => $data[0],
                 "nombrefantasia" => $data[2],
                 "direccion" => str_replace("-", "", $data[3]),
                 "codigopostal" => $data[4],
-                "provincia" => $this->getProvinciaReference(trim($data[6])),
-                "ciudad_id" => $localidad,
-                "Rubro" => array(
-                    "id" => $this->getRubroReference($data[1])
-                )
+                "provincia" => $provincia_id,
+                "ciudad_id" => $localidad
             );
+            if ($rubro > 0) {
+                $new_comerce['Rubro']['id'] = $rubro;
+            }
             $this->Comercio->create();
             //debug($new_comerce);
             $saved = $this->Comercio->save($new_comerce);
-            $this->out("<flashy>Insertado</flashy> ".$saved['Comercio']['nombrefantasia']);
+            $this->out("<flashy>Insertado</flashy> " . $saved['Comercio']['nombrefantasia']);
+            //$this->out("<flashy>Insertado</flashy> ");
         }
     }
 
-    private function populateRubros() {
+    private function _populateRubros() {
         $this->rubros_reference[180] = 7;
         $this->rubros_reference[130] = 2;
         $this->rubros_reference[610] = 18;
@@ -94,7 +105,7 @@ class TransactionIntegrationShell extends AppShell {
         $this->rubros_reference[510] = 16;
     }
 
-    private function populateProvinces() {
+    private function _populateProvinces() {
         $this->provincias_reference[1] = 5;
         $this->provincias_reference[2] = 1;
         $this->provincias_reference[3] = 2;
@@ -121,38 +132,16 @@ class TransactionIntegrationShell extends AppShell {
         $this->provincias_reference[24] = 24;
     }
 
-    private function getRubroReference($external_id) {
+    private function _getRubroReference($external_id) {
         return $this->rubros_reference[$external_id];
     }
 
-    private function getProvinciaReference($external_id) {
+    private function _getProvinciaReference($external_id) {
 
         return $this->provincias_reference[$external_id];
     }
 
-    private function processLine($data) {
-        $comercios = $this->Comercio->find('all', array(
-            'conditions' => array('Comercio.cuit' => $data[0])
-        ));
-        if (!$comercios) {
-            $this->insertNewComerce($data);
-        } 
-    }
-
-    private function updateExisting($comerces_by_cuit, $data) {
-        foreach ($comerces_by_cuit as $c) {
-            if ($c['Comercio']['activo'] == false) {
-                $this->out("debo activar ");
-            }
-            //si codigo postal es igual y no tiene rubro
-            if (isset($c['Rubro']) && count($c['Rubro']) == 0 && $data[4] == $c['Comercio']['codigopostal']) {
-                //debug($c);
-                $this->out("debo agregar rubro " . $this->getRubroReference($data[1]));
-            }
-        }
-    }
-
-    private function getLocalidad($dirty_name) {
+    private function _getLocalidad($dirty_name) {
 
         $ciudade = $this->Ciudade->find('first', array(
             'conditions' => array("Ciudade.nombre" => trim($dirty_name)),
@@ -165,7 +154,7 @@ class TransactionIntegrationShell extends AppShell {
         }
 
 
-        return $ciudade ? $ciudade['Ciudade']['id'] : false;
+        return $ciudade ? $ciudade['Ciudade']['id'] : 1;
     }
 
 }
