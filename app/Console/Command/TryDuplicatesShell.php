ef7c3487fab8936d743a346e5ae821c22553a6d4
Sebastian Perez Leale <sebartista@gmail.com>
2019-07-08 19:28:19 -0300

    proceso modificado

--- app/Console/Command/TryDuplicatesShell.php
+++ app/Console/Command/TryDuplicatesShell.php
@@ -3,16 +3,17 @@
 class TryDuplicatesShell extends AppShell {
 
     private $ids_revisados = array();
+    private $ids_desactivar = array();
     public $uses = array('Comercio');
     private $counter = 0;
     
     public function main() {
         $comercios_count = $this->Comercio->find('count');
         while($this->counter < $comercios_count){
+            
             $this->getDuplicates();
             $this->counter++;
         }
-        
     }
 
     private function getDuplicates() {
@@ -20,7 +21,7 @@ class TryDuplicatesShell extends AppShell {
             'conditions' => array(
                 'Comercio.id !=' => $this->ids_revisados,
                 'Comercio.activo' => true),
-            'order' => array('Comercio.id ASC'),
+            //'order' => array('Comercio.id ASC'),
             'recursive' => 0
         ));
         if ($comercio) {
@@ -32,11 +33,17 @@ class TryDuplicatesShell extends AppShell {
                     'Comercio.codigopostal' => $comercio['Comercio']['codigopostal'],
                     'Comercio.direccion' => $comercio['Comercio']['direccion']
                 ),
-                'order' => array('Comercio.id ASC')
+                'recursive' => 0
+                //'order' => array('Comercio.id ASC')
             ));
             if (count($duplicates) > 1) {
-                $this->out('<info> ID principal '.$comercio['Comercio']['id']);
+                //$this->out('<info> ID principal '.$comercio['Comercio']['id']);
                 $this->out('<info>encontre </info>' . count($duplicates));
+                if(count($this->ids_desactivar) > 1000){
+                    $this->out('<info>procesados </info>' . $this->counter);
+                    
+                    $this->desactivarBulk();
+                }
                 $this->desactivarDuplicados($duplicates);
             }
 
@@ -46,15 +53,32 @@ class TryDuplicatesShell extends AppShell {
     }
 
     private function desactivarDuplicados($duplicates) {
-        $desactivar = array();
-        foreach ($duplicates as &$comercio) {            
+        
+        foreach ($duplicates as $comercio) {            
             //quedaria desactivado por lo cual salteo la condicion de ids_revisados
-            $comercio['Comercio']['activo'] = false;
-            $desactivar[] = $comercio;
-            $this->out("<warning>Desactivando:</warning> ".$comercio['Comercio']['id']);
+            
+            $this->ids_desactivar[] = $comercio['Comercio']['id'];
+            $this->ids_revisados[] = $comercio['Comercio']['id'];
+            //$this->out("<warning>Desactivando:</warning> ".$comercio['Comercio']['id']);
         }
         
         //$this->Comercio->saveMany($desactivar, array('validate' => false));
+    }
+    
+    private function desactivarBulk(){
+        
+        $this->out('desactivando BULK');
+        $fields = array('Comercio.activo' => false);
+        $conditions = array('Comercio.id' => $this->ids_desactivar);
+        $this->Comercio->updateAll($fields, $conditions);
+        $comercio_test = $this->Comercio->find('first', 
+                array('conditions' => array(
+                    'Comercio.id' => $this->ids_desactivar[0]
+                )
+                    ));
+        $this->out('<info>Test desactivar</info> Deberia ser false'. $comercio_test['Comercio']['activo']);
+        //debug($comercio_test);
+        $this->ids_desactivar = array();
     }
 
 }
